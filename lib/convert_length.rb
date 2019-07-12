cb4e2a43bd3b5ab188ef4500e333e622b483bf80
yuu0305 <segyayuu8@gmail.com>
2019-07-12 20:44:57 +0900

    add_New_files
    ファイル追加

--- lib/convert_length.rb
+++ lib/convert_length.rb
@@ -1,6 +1,9 @@
-def convert_length(length ,from: :m ,to: :m)
-  units = {m: 1.0, ft: 3.28, in: 39.37}
-  (length/ units[from] * units[to]).round(2)
-end
+# def convert_length(length ,from: :m ,to: :m)
+#   units = {m: 1.0, ft: 3.28, in: 39.37}
+#   (length/ units[from] * units[to]).round(2)
+# end
 
 
+t = '123 456 789'.scan(/\d/)
+
+p t
