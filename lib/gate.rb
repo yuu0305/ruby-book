3c99f22547af0e0c2e453b5a247c08e2baaf083e
yuu0305 <segyayuu8@gmail.com>
2019-07-14 02:00:36 +0900

    practice
    クラスメソッド練習

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,5 +1,21 @@
+class User
+  def initialize(name)
+    @name = name
+  end
 
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
 
-class 
-  
+  def hello
+    "Hello, I am #{@name}. "
+  end
+end
+
+names = ['Alice' , 'Bob' , 'Carol']
+users = User.create_users(names)
+users.each do |user|
+  puts user.hello
 end
