c1d126bf38b72b73ad1b37a58462df9e0b696c26
yuu0305 <segyayuu8@gmail.com>
2019-07-18 14:37:42 +0900

    practice
    練習問題

--- lib/example.rb
+++ lib/example.rb
@@ -1,22 +1,51 @@
-class Fruits
-  @@sum = 0
+class Food
+  @@foods = []
 
-  attr_accessor :name,:price
+  def self.input
+    puts "ご飯を入力してね"
+    food = gets.to_s
+    puts "カロリーを入力してね"
+    calory = gets.to_i
 
-  def initialize(name,price)
-    @name = name
-    @price = price
-    @@sum += price
+    food = Food.new(food ,calory)
+    @@foods << food
+    return
   end
-  
-  def self.get_sum
-    "合計金額は#{@@sum}です"
+
+  def self.show_all_calory
+    sum = 0
+    @@foods.each do |foo|
+      puts "#{foo.food}:#{foo.calory}"
+      sum += foo.calory
+    end
+    puts "_______________________"
+    puts "合計カロリー: #{sum}"
+  end
+
+  def initialize(food,calory)
+    @food = food
+    @calory = calory
+  end
+
+  def food
+    return @food
+  end
+
+  def calory
+    return @calory
   end
 
 end
 
-a = Fruits.new("apple",120)
-b = Fruits.new("orange",200)
-c = Fruits.new("starrawberry",60)
+while true do
+  puts "[0]:カロリーを入力する"
+  puts "[1]:カロリーの合計を見る"
+  input = gets.to_i
 
-puts Fruits.get_sum
+  if input == 0
+    Food.input           # カロリーの入力
+  elsif input == 1
+    Food.show_all_calory # カロリーの合計を表示
+    exit
+  end
+end
