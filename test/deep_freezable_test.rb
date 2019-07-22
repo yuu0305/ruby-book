d3b3e133c6976ee4e28d80c5fcf5ea1b58c93140
yuu0305 <segyayuu8@gmail.com>
2019-07-22 18:27:44 +0900

    temporary
    仮

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -3,7 +3,9 @@ require '../lib/bank'
 require '../lib/team'
 
 class DeepFreezableTest < Minitest::Test
-  def test_deep_freeze
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    assert_equal ['Japan' , 'US' , 'India'], Team::COUNTRIES
+    assert Team::COUNTRIES.frozen?
+    assert Team::COUNTRIES.all?{|country| country.frozen? }
   end
 end
