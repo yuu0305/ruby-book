b0d6c32f039b71a05238114848175d96d7e15510
yuu0305 <segyayuu8@gmail.com>
2019-07-29 17:07:40 +0900

    matches
    Matches

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -2,3 +2,11 @@ print 'Text?:'
 text = gets.chomp
 print 'Pattern?:'
 pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched: #{matches.join(', ')}"
+else
+  puts "Nothing matched."
+end
