86ded4b2de7406d5ded660c3a4d3d6da442b69a0
yuu0305 <segyayuu8@gmail.com>
2019-07-30 23:03:39 +0900

    yield
    yield

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,12 +1,9 @@
-first =  gets.to_i
-second = gets.to_i
-
-begin
-  puts "計算結果:[#{first/second.to_f}]...calc成功です。"
-rescue NoMethodError
-  puts  "NoMethodError"
-rescue NameError
-  puts "NameError"
-rescue StandardError
-  puts  "その他のエラーです。"
+def greeting
+  puts "hello"
+  yield 
+  puts "good night"
 end
+
+greeting do
+puts "good morning"
+end 
