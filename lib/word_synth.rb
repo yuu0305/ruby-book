0dcf118821875792fb7da73ed85a57674fef8472
yuu0305 <segyayuu8@gmail.com>
2019-08-01 01:51:19 +0900

    word synth success
    success

--- lib/word_synth.rb
+++ lib/word_synth.rb
@@ -1,5 +1,16 @@
 class WordSynth
-  def play(original_words)
-    
+  def initialize
+    @effects = []
   end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
+  def play(original_words)
+    @effects.inject(original_words) do |words,effect|
+      effect.call(words)
+    end
+  end
+
 end
--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -3,9 +3,9 @@ require '../lib/word_synth'
 require '../lib/effects'
 
 
-class WordSynthTest <  Minitest::Test
-  def test_play
-    assert WordSynth
-    assert Effects
+class WordSynthTest < Minitest::Test
+  def test_play_with_effects
+    synth = WordSynth.new
+    assert_equal 'Ruby is fun!' , synth.play('Ruby is fun!')
   end
 end
